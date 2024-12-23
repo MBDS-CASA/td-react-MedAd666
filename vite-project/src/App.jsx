281a9eab592ed28afbd2990ffa0d5759d29876b5
adarrab <adarrabamine1@gmail.com>
2024-12-23 15:33:17 +0100

    TD01 : Ajouter un footer

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -21,6 +21,12 @@ function MainContent(){
     )
 }
 
+function Footer(){
+    return (
+        <p>Tous droits réservés - Adarrab Mohammed Amine</p>
+    )
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -48,6 +54,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+        <Footer/>
     </>
   )
 }
